b45a2954e832a5816d1dd252bfd242a0bf6d11a2
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2021-02-09 00:21:40 +0100

    trial and error

--- js/lib/example.js
+++ js/lib/example.js
@@ -1,5 +1,6 @@
 var widgets = require('@jupyter-widgets/base');
-import "../node_modules/cesium/Build/Cesium/Widgets/widgets.css";
+//import "../node_modules/cesium/Build/Cesium/Widgets/widgets.css";
+import "../node_modules/cesium/Source/Widgets/widgets.css"
 
 // All kind of trial'n errors
 //const CESIUM_BASE_URL = 'https://localhost:5000/';
@@ -20,12 +21,15 @@ import "../node_modules/cesium/Build/Cesium/Widgets/widgets.css";
 //import "cesium/Build/Cesium/Widgets/widgets.css";
 //import "cesium/Build/Cesium/Widgets/widgets.css";
 //import "./css/main.css";
+window.CESIUM_BASE_URL = '';
 
 import * as Cesium from 'cesium';
 import { Color } from 'cesium';
 import { Cesium3DTileset, createWorldTerrain, IonResource, Viewer } from 'cesium';
 
 var _ = require('lodash');
+
+var uniquePointer = 0;
 
 // See example.py for the kernel counterpart to this file.
 
@@ -63,26 +67,33 @@ export var HelloView = widgets.DOMWidgetView.extend({
     // Defines how the widget gets rendered into the DOM
     render: function() {
         console.log('start my render');
+        HelloView.__super__.render.apply(this, arguments);
+
         //const CESIUM_BASE_URL = 'https://cesium.com/downloads/cesiumjs/releases/1.78/Build/Cesium/'; 
 
-        this.value_changed();
+        //this.value_changed();
       
         
         var c = Color.fromRandom();
-
         this.cesiumContainer = document.createElement("div");
-        this.cesiumContainer.setAttribute("class", "cesiumContainer");
-        this.cesiumContainer.setAttribute('width', '100%');
-        this.cesiumContainer.setAttribute('height', '200');
+        var id = "cesiumContainer" + uniquePointer;
+        uniquePointer++;
+
+        this.cesiumContainer.setAttribute("class", id);
+        this.cesiumContainer.style.width = '100%';
+        this.cesiumContainer.style.height = '600px';
+        this.cesiumContainer.style.padding = '0';
+
         this.el.appendChild(this.cesiumContainer);
         var viewer = new Viewer(this.cesiumContainer, {
             shouldAnimate: true,
           });
         this.cesiumViewer = viewer;
+        this.cesiumViewer.fullscreenButton.viewModel.fullscreenElement = this.cesiumViewer.container.childNodes[0];
+
         console.log(viewer);
 
         
-
         // Observe changes in the value traitlet in Python, and define
         // a custom callback.
         this.model.on('change:value', this.value_changed, this);
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -5,6 +5,8 @@ var version = require('./package.json').version;
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+
 const webpack = require('webpack');
 
 // The path to the CesiumJS source code
@@ -19,17 +21,35 @@ var rules = [{
     use: ['style-loader', 'css-loader']
     }, {
     test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-    type:'asset/inline'
-}];
+    type:'asset/inline'},
+    { test: /\.json$/, loader: 'json-loader' },
+    //{ test: /\.js$/, loader: 'babel-loader', query: {presets: ['es2015', 'stage-0']}, exclude: /node_modules/ }
+    {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    // {
+    //     test: /\.js$/,
+    //     exclude: "/node_modules/",
+    //     use: "babel-loader",
+    //   },
+
+];
 
 var plugins = [
     new HtmlWebpackPlugin({
-        template: 'src/index.html'
+        template: path.resolve(__dirname, 'src', 'index.html') //'src/index.html'
     }),
     
     new webpack.DefinePlugin({
         // Define relative base path in cesium for loading assets
-        'CESIUM_BASE_URL': JSON.stringify("../")
+        CESIUM_BASE_URL: JSON.stringify("")
     }), 
     new CopyWebpackPlugin({
         patterns: [
@@ -38,7 +58,18 @@ var plugins = [
             { from: path.join(cesiumSource, "Assets"), to: 'Assets' },
             { from: path.join(cesiumSource, "Widgets"), to: 'Widgets' }
         ],
-    })
+    }),
+    new HtmlWebpackTagsPlugin({ tags: ['cesium/Cesium.js', 'cesium/Widgets/widgets.css'], append: false }),
+
+    new CopyPlugin({
+        patterns: [
+          {
+            from: `../node_modules/cesium/Build/Cesium${prod ? "" : "Unminified"}`,
+            to: "cesium",
+          },
+        ],
+      }),
+      
 ];
 
 
@@ -59,19 +90,10 @@ module.exports = (env, argv) => {
                 path: path.resolve(__dirname, '..', 'cesiumwidget3', 'nbextension'),
                 libraryTarget: 'amd',
                 publicPath: '', // publicPath is set in extension.js
-                sourcePrefix: ''
+  
             },
-            devtool,
-            amd: {
-                // Enable webpack-friendly use of require in Cesium
-                toUrlUndefined: true
-            },
-            plugins: plugins,
-            module: {
-                unknownContextCritical: false,
-                rules: rules
-            },
-            
+            devtool: 'eval',
+            plugins: plugins,         
         },
         {// Bundle for the notebook containing the custom widget views and models
         //
@@ -93,7 +115,7 @@ module.exports = (env, argv) => {
                 // Enable webpack-friendly use of require in Cesium
                 toUrlUndefined: true
             },
-            devtool,
+            devtool: 'eval',
             module: {
                 unknownContextCritical: false,
                 rules: rules
@@ -137,7 +159,7 @@ module.exports = (env, argv) => {
                 // Needed to compile multiline strings in Cesium
                 sourcePrefix: ''
             },
-            devtool,
+            devtool: 'eval',
             amd: {
                 // Enable webpack-friendly use of require in Cesium
                 toUrlUndefined: true
@@ -168,7 +190,7 @@ module.exports = (env, argv) => {
             },
             output: {
                 filename: '[name].js',
-                path: path.resolve(__dirname, '..', 'cesiumwidget3', 'nbextension'),
+                path: path.resolve(__dirname,'dist' ) //'..', 'cesiumwidget3', 'nbextension'),
             },
             devtool: 'eval',
             node: {
@@ -195,8 +217,13 @@ module.exports = (env, argv) => {
                          https: "empty",
                          zlib: "empty"
                      },
+                     //alias: {
+                        // CesiumJS module name
+                       // cesium: path.resolve(__dirname, cesiumSource)
+                    //}
             },
             module: {
+                unknownContextCritical: false,
                 rules: rules,
            
             },
